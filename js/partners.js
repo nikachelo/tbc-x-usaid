159b42119c91819d4960e149dc6a1ba6570de56c
Chelo <nikolozi.tchelidze192@ens.tsu.ge>
2024-01-26 16:37:31 +0400

    Edited instructionos for adding partners correctly

--- js/partners.js
+++ js/partners.js
@@ -1,6 +1,8 @@
 /* აქედან შესაძლებელია პარტნიორების დინამიურად დამატება,
    სლაიდერს გვერდები ემატება პარტნიორების რაოდენობის მიხედვით, ყოველ 3 პარტნიორზე თითო გვერდი */
 
+/* ------ პარტნიორები დინამიურად ------ */
+
 const partnersData = [
   { name: "Partner 1", image: "images/partners/space.png" },
   { name: "Partner 2", image: "images/partners/tegeta.png" },
@@ -9,6 +11,8 @@ const partnersData = [
   { name: "Partner 6", image: "images/partners/spectre.png" },
   { name: "Partner 8", image: "images/partners/ufc.png" },
 ];
+
+/* ------ დასასრული ------ */
 
 const partnersPerPage = 3;
 const totalSlides = Math.ceil(partnersData.length / partnersPerPage);
