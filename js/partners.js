c2591c10d02d1e613d2c34a46d5d92d87efcfe8b
Chelo <nikolozi.tchelidze192@ens.tsu.ge>
2024-01-28 15:43:39 +0400

    banner responsiveness add

--- js/partners.js
+++ js/partners.js
@@ -4,12 +4,13 @@
 /* ------ პარტნიორები დინამიურად ------ */
 
 const partnersData = [
-  { name: "Partner 1", image: "images/partners/space.png" },
-  { name: "Partner 2", image: "images/partners/tegeta.png" },
-  { name: "Partner 3", image: "images/partners/tnet.png" },
-  { name: "Partner 4", image: "images/partners/usaid.png" },
-  { name: "Partner 6", image: "images/partners/spectre.png" },
-  { name: "Partner 8", image: "images/partners/ufc.png" },
+  { name: "Usaid", image: "images/partners/usaid.png" },
+  { name: "Space", image: "images/partners/space.png" },
+  { name: "Tnet", image: "images/partners/tnet.png" },
+  { name: "Tegeta", image: "images/partners/tegeta.png" },
+  { name: "Spectre", image: "images/partners/spectre.png" },
+  { name: "TBC", image: "images/partners/tbc.png" },
+  { name: "UFC", image: "images/partners/ufc.png" },
 ];
 
 /* ------ დასასრული ------ */
